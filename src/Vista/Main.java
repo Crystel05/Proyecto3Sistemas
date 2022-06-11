a07e5841224340928cba870ad27f373f86cb42ec
Crystel Montero <crysvane05@estudiantec.cr>
2022-06-10 18:31:56 -0600

    prueba 2

--- src/Vista/Main.java
+++ src/Vista/Main.java
@@ -4,5 +4,6 @@ public class Main {
     public static void main(String[] args){
         System.out.println("Mishis love");
         System.out.println("HOLA");
+        System.out.println("HOLA 2");
     }
 }
