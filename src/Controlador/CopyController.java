fa00423b5065eb6afafb3f60bccd39649812b5b4
Crystel Montero <crysvane05@estudiantec.cr>
2022-06-19 15:38:17 -0600

    copies

---
+++ src/Controlador/CopyController.java
@@ -0,0 +1,12 @@
+package Controlador;
+
+import java.io.File;
+
+public class CopyController {
+
+    private void copy(String pathOrigin, String pathDestiny, boolean isDirectory){
+        if (!isDirectory){
+            File file = new File();
+        }
+    }
+}
